02fc08dbe1d3375122f90a7630332b240bdfb287
master <anishmjain2003@gmail.com>
2023-10-18 22:42:48 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export const About = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>
+        Hi there! I'm, a dedicated and enthusiastic web developer.I love bringing creativity,
+        attention to detail, and a passion for crafting seamless user experiences.
+      
+      </p>
+      
+    </section>
+  );
+};
+
---
+++ src/components/Projects.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <section id="projects">
+      <h2>Projects</h2>
+      {/* Add your project components or details here */}
+      <h1>COMING SOON...</h1>
+      
+      
+
+
+    </section>
+  );
+}
+
+export default Projects;
